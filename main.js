e605fe414d417376313b483da9535696f9f67ff8
nattadetspite <ntd.ctnts@gmail.com>
2021-08-08 03:13:27 +0700

    test button

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("test");
+
+window.addEventListener("load", () => {
+  const testButton = document.getElementById("test-button");
+  testButton.addEventListener("click", () => {
+    console.log("hello_test");
+    // connect web with camera
+    // capture
+    // process image
+    // result
+    // display result image
+  });
+});
